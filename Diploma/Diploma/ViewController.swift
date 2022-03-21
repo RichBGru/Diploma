3ac0df256d319cc81371ec8d1402d76a32a44794
Artem Tkachuk <astkachuk@domclick.ru>
2022-03-22 00:21:15 +0300

    Initial commit

---
+++ Diploma/Diploma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diploma
+//
+//  Created by Ткачук Артем Сергеевич on 21.03.2022.
+//
+
+import UIKit
+
+class ScheduleMainTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
